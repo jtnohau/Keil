1723c9b479078ebf809f065647306ba7851e2671
Jonas T <jt@nohau.se>
2022-05-11 14:28:53 +0200

    blyatt

--- MDK/Boards/Keil/MCBSTM32F400/Blinky/main.c
+++ MDK/Boards/Keil/MCBSTM32F400/Blinky/main.c
@@ -100,7 +100,7 @@ int main(void)
      */
   HAL_Init();
 	
-	printf("test2 \n");
+	printf("On newbranch push to main \n");
 
   /* Configure the system clock to 168 MHz */
   SystemClock_Config();
